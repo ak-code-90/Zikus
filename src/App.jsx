36a9ca73061d693ec1b95a73813b702bfcac50cc
Kevin <adda-kevin@hotmail.fr>
2022-09-21 23:11:34 +0200

    add landing page

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [appMessage, setAppMessage] = useState('Get Started')
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://developer.spotify.com/discover/" target="_blank">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1982px-Spotify_icon.svg.png" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Zikus <br></br> <span className='slogan'>Écoutez Partagez Vibrez</span> </h1>
+      <div className="card">
+        <button onClick={() => setAppMessage((appMessage) => "Comming Soon")}>
+          {appMessage}
+        </button>
+        <h2>Le meilleur de la musique, ensemble </h2>
+        <p>
+          Partager des playlists spotify avec vos amis, commenter les nouveaux sons à la une !
+          et recevez des notifications lorsque vos playlists sont mises à jour
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Cette application est conçu avec React ainsi que l'API de spotify,
+        cliquer sur les logos pour en apprendre plus sur ces technologies 
+      </p>
+    </div>
+  )
+}
+
+export default App
