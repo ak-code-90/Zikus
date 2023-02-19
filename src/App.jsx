266c2faec609d737176f427d01245e2f6d7eebd6
Kevin <adda-kevin@hotmail.fr>
2023-02-19 02:30:17 +0100

    user authorization request added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import PlaylistsPage from './pages/PlaylistsPage'
 
 function App() {
-  const [appMessage, setAppMessage] = useState('Get Started')
+
+  const client_id = '435a00f8faec4c7f9dbe6186a0d606b7';
+  const client_secret = '1a398417576c48b08b445f0d1e7903cb';
+  const redirect_uri = 'http://localhost:5173/playlists';
+  const authorization_code = 'AUTHORIZATION_CODE';
+  const scope = 'user-read-email user-read-private user-library-modify user-library-read playlist-modify-public user-read-playback-position user-top-read user-read-recently-played user-follow-modify user-follow-read playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public app-remote-control streaming user-read-playback-state user-modify-playback-state user-read-currently-playing ugc-image-upload'
+  const state = 'fklzjlkcel132sukizdjezk12'
+
+  const getAccessToken = () => {
+    window.location = `https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&state=${state}&scope=${scope}&show_dialog=true`
+
+  };
+
+
 
   return (
     <div className="App">
@@ -19,10 +29,7 @@ function App() {
       </div>
       <h1>Zikus <br></br> <span className='slogan'>Écoutez Partagez Vibrez</span> </h1>
       <div className="card">
-        <button onClick={() => setAppMessage("Coming Soon")}>
-          <Link to={'/playlists'}>{appMessage}</Link>
-        </button>
-
+        <button onClick={getAccessToken}>Get Started</button>
         <p style={{ fontSize: '21px' }}>
           Partagez des playlists spotify avec vos amis, commentez les nouveaux sons à la une !
           <br /> Et recevez des notifications lorsque vos playlists sont mises à jour
--- src/pages/PlaylistsPage.jsx
+++ src/pages/PlaylistsPage.jsx
@@ -1,6 +1,35 @@
+import React, { useEffect, useState } from 'react';
 import '../styles/PlaylistsPage.css'
+// import SpotifyWebApi from 'spotify-web-api-js'
+
+
 
 export default function PlaylistsPage() {
+
+    const [allPlaylists, setAllPlaylists] = useState([]);
+
+    // const spotifyApi = new SpotifyWebApi({
+    //     clientId: '435a00f8faec4c7f9dbe6186a0d606b7',
+    //     clientSecret: '1a398417576c48b08b445f0d1e7903cb'
+    // })
+
+    // const myToken = 'BQAKDtmO7sbWR4EjgaPOT3duvW66Reu3vRIRozF9xTAjtaaKRZJmQ559kA3Z8iPytFqAymtRtRGz9E28ss-UpRm_-PndHcnASa3yACo5ay4VpgStFxkHbQG73Zrw06IyVOKtD4rnHbZxNopZWOJmxHdsEiYIowWA8Qrhf7r_wQGYeWgNwvOPxKxOZCP9DsfY_JP6-WXma6uwz8DMCedGLddnD7e1vuudyRSweSoeP2w8G3k7DQD6m-5LGhgMifCsQwi3aAfXSpKZNlg0iFZcZUGS02bE8VUQbVBTYRKJpUCkic45uO1iV0c2Tg'
+
+    // async function getAllPlaylists() {
+    //     spotifyApi.setAccessToken(myToken);
+    //     const { items } = await spotifyApi.getUserPlaylists();
+    //     setAllPlaylists(items)
+    // }
+
+    // useEffect(() => {
+    //     getAllPlaylists();
+    //     console.log(allPlaylists);
+    // }, [])
+
+    // useEffect(() => {
+    //     console.log(allPlaylists);
+    // }, [allPlaylists])
+
     return (
         <div className='globalContainer'>
             Hello World
